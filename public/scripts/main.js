f58a8de4d4213b32213ee4c97f78f528bdaf55a7
kenji <indytrading@gmail.com>
2016-05-09 11:23:43 +0700

    initial commit

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,17 @@
-"use strict";
+// "use strict";
+//
+//   $('.navbar-mobile button span').click(function() {
+//       $(this).children('.caret').toggleClass("rotate-180");
+//       $(this).closest('.nav').find('button a').not(this).children('.caret').removeClass("rotate-180");
+//   });
+//
+//   $(function () {
+//   $(".news-post_thumbnail").slice(0, 8).show();
+//   $("#load").on('click', function (e) {
+//       e.preventDefault();
+//       $(".news-post_thumbnail:hidden").slice(0, 8).slideDown();
+//       if ($(".news-post_thumbnail:hidden").length == 0) {
+//           $("#load").fadeOut('slow');
+//       }
+//   });
+// });
