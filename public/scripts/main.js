902b3f946288ca463f2e0063bd04b90dd2f62007
kenji <indytrading@gmail.com>
2016-05-16 21:39:43 +0700

    initial commit

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,18 +1,21 @@
-// Rotaet Dropdown
-  $('.navbar-mobile button span').click(function() {
-    $(this).children('.caret').toggleClass("rotate-180");
-    $(this).closest('.nav').find('button a').not(this).children('.caret').removeClass("rotate-180");
-  });
+'use strict';
 
-  $(".navbar-nav_desktop li a").on("click", function() {
-    $("a").find(".active").removeClass("active");
-    $(this).parent().addClass("active");
-  });
-  ('.btn-member_menu').click(function() {
-    $(this).children('.caret').toggleClass("rotate-180");
-    $(this).closest('.nav').find('button a').not(this).children('.caret').removeClass("rotate-180");
-  });
-  
+// Rotaet Dropdown
+$('.navbar-mobile button span').click(function () {
+  $(this).children('.caret').toggleClass("rotate-180");
+  $(this).closest('.nav').find('button a').not(this).children('.caret').removeClass("rotate-180");
+});
+
+$(".navbar-nav_desktop li a").on("click", function () {
+  $("a").find(".active").removeClass("active");
+  $(this).parent().addClass("active");
+});
+'.btn-member_menu'.click(function () {
+  $(this).children('.caret').toggleClass("rotate-180");
+  $(this).closest('.nav').find('button a').not(this).children('.caret').removeClass("rotate-180");
+});
+
 // Lightbox
 var lightbox = new Lightbox();
 lightbox.load();
+//# sourceMappingURL=main.js.map
