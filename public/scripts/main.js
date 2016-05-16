43577376c9a05f7d6adc7ee2bb0875b940319e88
kenji <indytrading@gmail.com>
2016-05-16 21:27:56 +0700

    initial commit

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,2 +1,18 @@
-"use strict";
-//# sourceMappingURL=main.js.map
+// Rotaet Dropdown
+  $('.navbar-mobile button span').click(function() {
+    $(this).children('.caret').toggleClass("rotate-180");
+    $(this).closest('.nav').find('button a').not(this).children('.caret').removeClass("rotate-180");
+  });
+
+  $(".navbar-nav_desktop li a").on("click", function() {
+    $("a").find(".active").removeClass("active");
+    $(this).parent().addClass("active");
+  });
+  ('.btn-member_menu').click(function() {
+    $(this).children('.caret').toggleClass("rotate-180");
+    $(this).closest('.nav').find('button a').not(this).children('.caret').removeClass("rotate-180");
+  });
+  
+// Lightbox
+var lightbox = new Lightbox();
+lightbox.load();
