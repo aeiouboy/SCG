7d80ec2c03abf80b302438afed5109823368cdd6
kenji <indytrading@gmail.com>
2016-06-07 12:29:51 +0700

    initial commit

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,26 +1,2 @@
-'use strict';
-
-// Rotate Dropdown
-$(document).ready(function () {
-  $('.navbar-mobile button span, .dropdown-toggle, button').click(function () {
-    $(this).children('.caret').toggleClass("rotate-180");
-    $(this).closest('.nav').find('button a, a').not(this).children('.caret').removeClass("rotate-180");
-  });
-
-  $(".navbar-nav_desktop li a").on("click", function () {
-    $("a").find(".active").removeClass("active");
-    $(this).parent().addClass("active");
-  });
-
-  $('.carousel').carousel({
-  interval: 5000
-});
-
-if ( ($(window).height() + 800) < $(document).height() ) {
-$('#top-link-block').removeClass('hidden').affix({
-    offset: {top:800}
-});
-}
-
-
-});
+"use strict";
+//# sourceMappingURL=main.js.map
